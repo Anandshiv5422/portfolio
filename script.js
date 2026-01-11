e0668c1857619a56f7a04c608fc1d5017f41ccda
anandb <158706801+Anandshiv5422@users.noreply.github.com>
2026-01-11 14:45:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
-    });
-});
+
